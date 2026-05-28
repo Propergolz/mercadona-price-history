960af2276f24dd45639fe3ee65eb57d12241c8b5
Propergolz <sergio@ayudaexcel.com>
2026-05-28 13:54:04 +0200

    Update config.py

--- src/mercadona_history/config.py
+++ src/mercadona_history/config.py
@@ -11,6 +11,10 @@ class Location:
 
 
 DEFAULT_LOCATIONS = [
+    Location(location_id="madrid_28001", province="Madrid", postal_code="28001"),
+    Location(location_id="barcelona_08001", province="Barcelona", postal_code="08001"),
     Location(location_id="valencia_46001", province="Valencia", postal_code="46001"),
+    Location(location_id="alicante_03001", province="Alicante", postal_code="03001"),
+    Location(location_id="sevilla_41001", province="Sevilla", postal_code="41001"),
 ]
 
